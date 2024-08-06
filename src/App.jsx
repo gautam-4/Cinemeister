5f6b0a3373067cbe8865a4458589e0d1859b4d0b
gautam-4 <gautamalt468@gmail.com>
2024-08-06 15:15:44 +0530

    change api url

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './main.css'
 
 function App() {
 
-  const API_URL = `http://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&`
+  const API_URL = `https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&`
 
   const [movies, setMovies] = useState([]);
   const [searchItem, setSearchItem] = useState();
